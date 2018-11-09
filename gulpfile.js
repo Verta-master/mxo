b06c438485f8d68d957304b512a6c9495042a538
Verta-master <vertyagina@gmail.com>
2018-11-09 16:24:39 +0600

    Video

--- gulpfile.js
+++ gulpfile.js
@@ -88,9 +88,9 @@ gulp.task("js:production", function () {
 gulp.task("copy", function () {
   return gulp.src([
     "fonts/**/*",
-    "img/*.{svg,png,jpg,gif}",
+    "img/*.{svg,png,jpg,gif,mp4}",
     "js/*.js",
-	"css/*.css",
+	  "css/*.css",
     "*.txt",
     "*.html"
   ], {
